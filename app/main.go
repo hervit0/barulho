82e52536173e42be5f2df36333001cb6228fe939
Hervé Ah-Leung <herve.ahleung@gmail.com>
2019-09-26 12:19:00 +0100

    HAL > Attempt to populate database

--- app/main.go
+++ app/main.go
@@ -25,23 +25,33 @@ func main() {
 	//}
 
 	db := repository.Connect()
-	db.AutoMigrate(&models.Video{}, &models.Artist{}, &models.City{}, &models.Song{})
+	db.Debug().DropTableIfExists(&models.Video{}, &models.Artist{}, &models.City{}, &models.Song{})
+	db.Debug().AutoMigrate(&models.Video{}, &models.Artist{}, &models.City{}, &models.Song{})
 
 	for _, v := range videos {
 		if v.Song != nil {
 			if v.Song.Artist != nil {
-				db.FirstOrCreate(&v.Song.Artist, v.Song.Artist)
+				db.Debug().FirstOrCreate(&v.Song.Artist, v.Song.Artist)
 			}
 			if v.Song.City != nil {
-				db.FirstOrCreate(&v.Song.City, v.Song.City)
+				db.Debug().FirstOrCreate(&v.Song.City, v.Song.City)
 			}
-			db.FirstOrCreate(&v.Song, v.Song)
+			db.Debug().FirstOrCreate(&v.Song, v.Song)
 		}
-		db.FirstOrCreate(&v, v)
+		db.Debug().FirstOrCreate(&v, v)
 	}
 
+	var video models.Video
+	var song models.Song
+	var artist models.Artist
 	var city models.City
+	db.First(&video)
+	db.First(&song)
+	db.First(&artist)
 	db.First(&city)
 
+	log.Printf("%+v", video)
+	log.Printf("%+v", song)
+	log.Printf("%+v", artist)
 	log.Printf("%+v", city)
 }
